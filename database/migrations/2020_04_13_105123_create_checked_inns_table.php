14e89c75912f5bb8e5bc1fee95553cd21d42fea2
Leorne <leorneq@gmail.com>
2020-04-14 11:46:45 +0300

    result commit

--- database/migrations/2020_04_13_105123_create_checked_inns_table.php
+++ database/migrations/2020_04_13_105123_create_checked_inns_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCheckedInnsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('checked_inns', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('inn');
+            $table->boolean('status')->nullable();
+            $table->string('message');
+            $table->string('code')->nullable();
+            $table->smallInteger('response_status');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('checked_inns');
     }
 }
